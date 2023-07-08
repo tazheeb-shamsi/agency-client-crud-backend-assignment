60300b29b56f3c6334f5aabd90b32a17bdad59e9
Tazheeb Shamsi <thshamsi@hotmail.com>
2023-07-08 22:17:49 +0530

    added URL

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import { app } from "./app.js";
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server is running on port ${process.env.PORT}`)
-);
+const port = process.env.NODE_ENV === "production" ? process.env.PORT : 3000;
+const url =
+  process.env.NODE_ENV === "production"
+    ? "https://agency-client-crud-backend.onrender.com"
+    : `http://localhost:${port}`;
+app.listen(process.env.PORT, () => console.log(`Server is running at ${url}`));
